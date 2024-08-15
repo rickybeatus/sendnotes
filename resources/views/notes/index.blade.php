0ae740721b2efef0cf8eec3c3616e88676901635
ricky.beatus@gmail.com <ricky.beatus@gmail.com>
2024-08-15 17:20:39 +0700

    note creation and policies

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800">
-            {{ __('Dashboard') }}
+            {{ __('Notes') }}
         </h2>
     </x-slot>
 
@@ -9,7 +9,7 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <x-button primary label="Push Me..." />
+                    <livewire:notes.show-notes lazy />
                 </div>
             </div>
         </div>
